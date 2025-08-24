0e7c819f6dc5cda5b542a80b66ad807320d6d0ce
Petar Dragicevic <dragicevicpetar14@gmail.com>
2025-08-24 14:59:38 +0200

    [ADD]Factory class which represent factory method pattern

---
+++ GraphVisualizer/graph_explorer/plugins/factory.py
@@ -0,0 +1,31 @@
+from .data_source_plugin_json import JSONGraphParser
+from .data_source_plugin_xml import XmlFileParser
+from .data_source_plugin_yaml import YamlFileParser
+from .visualizer_block import BlockVisualizer
+from .visualizer_simple import SimpleVisualizer
+
+#FACTORY METHOD PATTERN
+class ParserFactory:
+    @staticmethod
+    def create_parser(file_name, driver, file_type):
+        file_type = file_type.lower()
+        if file_type == "json":
+            return JSONGraphParser(file_name, driver)
+        elif file_type == "xml":
+            return XmlFileParser(file_name, driver)
+        elif file_type == "yaml":
+            return YamlFileParser(file_name, driver)
+        else:
+            raise ValueError(f"Unsupported parser type: {file_type}")
+
+
+class VisualizerFactory:
+    @staticmethod
+    def create_visualizer(vis_type, graph_data=None):
+        vis_type = vis_type.lower()
+        if vis_type == "block":
+            return BlockVisualizer(graph_data)
+        elif vis_type == "simple":
+            return SimpleVisualizer()
+        else:
+            raise ValueError(f"Unsupported visualizer type: {vis_type}")

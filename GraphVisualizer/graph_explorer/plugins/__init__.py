fa5395427aa2d2ddfafdbfe468b551e21fc02434
Neven Ilincic <nevenilincic@gmail.com>
2025-08-25 18:33:15 +0200

    REMOVE factory folder

--- GraphVisualizer/graph_explorer/plugins/__init__.py
+++ GraphVisualizer/graph_explorer/plugins/__init__.py
@@ -3,7 +3,7 @@ from .data_source_plugin_yaml.yaml_data_source_plugin import YamlFileParser
 from .data_source_plugin_xml.xml_data_source_plugin import XmlFileParser
 from .visualizer_simple.simple_visualizer import SimpleVisualizer
 from .visualizer_block.block_visualizer import BlockVisualizer
-from .factory.factory import VisualizerFactory, ParserFactory
+from .factory import VisualizerFactory, ParserFactory
 from .base_parser import BaseParser
 
 __all__ = ["YamlFileParser", "JSONGraphParser", "XmlFileParser", "SimpleVisualizer", "BlockVisualizer", "VisualizerFactory", "ParserFactory", "BaseParser"]
